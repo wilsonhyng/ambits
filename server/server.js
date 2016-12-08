5706011db24fd98d86932e86bd60c5142edc152c
George A. Michel <sarumonakuba@gmail.com>
2016-12-07 18:35:56 -0800

    Verify mongo works with new schema and on Heroku, add db post testing path

--- server/server.js
+++ server/server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.set('port', (process.env.PORT || 3000));
+var mongoose = require('mongoose');
+
+// To use on Heroku, set the environment variable:
+// $ heroku set:config MONGOLAB_URL=mongodb://user:password@mongolabstuff
+var db = (process.env.MONGOLAB_URL || 'mongodb://localhost/ambits');
+
+mongoose.connect(db);
+var Location = require('./locations/locationSchema');
+
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname  +'/../client'));
@@ -13,7 +21,6 @@ app.set('view engine', 'html');
 
 // Should be refactored to go under /api
 // Also, probably, to be rehandled in an external routehandler/ctrlrs
-
 app.get('/locations', function(req, res) {
   res.send('locations go here');
 });
@@ -22,5 +29,32 @@ app.post('/locations', function(req, res) {
   res.send('something something');
 });
 
-app.listen(app.get('port'));
-console.log('Server is now listening at port ' + app.get('port'));
+// DB testing paths; remove when endpoints are built
+app.get('/db_post', function(req, res, next) {
+  var elapsed = Math.floor(Math.random()*100000);
+  var newLocation = new Location({
+    name: 'Testy McUserson',
+    geodata: elapsed
+  });
+  newLocation.save().then(function(newLocation) {
+    console.log('POSTED: ', newLocation);
+    res.json(newLocation);
+  }).catch(function(err) {
+    next(err);
+  });
+  
+});
+
+app.get('/db', function(req, res, next) {
+  Location.find().then(function(locations) {
+    res.json(locations);
+  })
+  .catch(function(err) {
+    next(err);
+  })
+});
+
+// To use on Heroku, must use port provided by process.env:
+var port = (process.env.PORT || 3000);
+app.listen(port);
+console.log('Server is now listening at port ' + port);
