0cf6a9c72f7763de877c913ee21b6f9c071e3687
wilsonhyng <wilsonhyng@gmail.com>
2016-12-14 15:51:41 -0800

    Add weekday display to individual ambits

--- client/src/app/pages/ambit.jsx
+++ client/src/app/pages/ambit.jsx
@@ -1,13 +1,23 @@
 import React            from 'react';
 import {Component}      from 'react';
 
+import AmbitWeekdays    from './ambitWeekdays.jsx'
+
 class Ambit extends Component {
   constructor(props) {
     super(props);
+    console.log(AmbitWeekdays);
   }
   
   render() {
-    
+    return (
+
+      <div>
+        <AmbitWeekdays />
+      </div>
+
+
+      )
   }
 }
 
---
+++ client/src/app/pages/ambitWeekdays.jsx
@@ -0,0 +1,102 @@
+import React            from 'react';
+import {Component}      from 'react';
+
+import injectTapEventPlugin
+                        from 'react-tap-event-plugin';
+
+
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+
+const fakeAmbit = {
+        refId: 1234,
+        name: 'Gym',
+        coords: {
+          latitude: 37.784,
+          longitude: -122.40903
+        },
+        weekdays:[true,true,false,true,true,false,false],
+        startDate:'2016-12-12',
+        checkIns:[]
+      };
+
+
+class AmbitWeekdays extends Component {
+  constructor(props) {
+    super(props);
+    console.log('imported');
+    this.state ={
+      weekdays: [true,true,false,true,true,false,false]
+    }
+  }
+  
+  render() {
+
+    const style = {
+      marginTop: 12,
+      marginRight: 10
+    };
+
+    const divStyle = {
+      textAlign: 'center'
+    };
+
+    function test() {
+      console.log('hello');
+    }
+
+    function changeAmbitDay() {
+      var ambitState = this.state; //state of current ambit
+      
+      // create utils function
+      // Utils.changeAmbitDay(ambitState, () => {
+      //   console.log('Changed Ambit Day')
+      // });
+
+
+      
+    }
+
+    function dayOnOff() {
+
+    }
+
+    return (
+// 
+      <div style={divStyle}>
+
+        <FloatingActionButton mini={true} disabled={!this.state.weekdays[0]}style={style} backgroundColor='purple' onTouchTap={test}>
+          <h1>M</h1>
+        </FloatingActionButton>
+
+        <FloatingActionButton mini={true} disabled={!this.state.weekdays[1]} style={style} backgroundColor='purple' onTouchTap={changeAmbitDay}>
+          <h1>Tu</h1>
+        </FloatingActionButton>
+
+        <FloatingActionButton mini={true} disabled={!this.state.weekdays[2]} style={style} backgroundColor='purple' onTouchTap={test}>
+          <h1>W</h1>
+        </FloatingActionButton>
+
+        <FloatingActionButton mini={true} disabled={!this.state.weekdays[3]} style={style} backgroundColor='purple' onTouchTap={test}>
+          <h1>Th</h1>
+        </FloatingActionButton>
+   
+        <FloatingActionButton mini={true} disabled={!this.state.weekdays[4]} style={style} backgroundColor='purple' onTouchTap={test}>
+          <h1>F</h1>
+        </FloatingActionButton>
+
+        <FloatingActionButton mini={true} disabled={!this.state.weekdays[5]} style={style} backgroundColor='purple' onTouchTap={test}>
+          <h1>Sa</h1>
+        </FloatingActionButton>
+
+        <FloatingActionButton mini={true} disabled={!this.state.weekdays[6]} style={style} backgroundColor='purple' onTouchTap={test}>
+          <h1>Su</h1>
+        </FloatingActionButton>
+
+      </div>
+      )
+  }
+}
+
+
+export default AmbitWeekdays;
