482adfafd0c18dd08f7c6ff480bad706d128076f
Vy Trinh <vy.trinh09@gmail.com>
2016-12-15 14:36:18 -0800

    (feat) Add floatingButtons for ambit icons

--- client/src/app/pages/week/dayOfWeek.jsx
+++ client/src/app/pages/week/dayOfWeek.jsx
@@ -5,7 +5,34 @@ import Ambit                from './ambitOfWeek.jsx'
 //material-ui
 import Paper                from 'material-ui/Paper';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
-import ContentAdd           from 'material-ui/svg-icons/content/add';
+
+
+const style = {
+  height: 100,
+  width: 100,
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block',
+};
+
+// const dayStyle = {
+//   textAlign: 'center',
+//   marginRight: 20
+// };
+
+// const ambitStyle = {
+//   float: 'left'
+// }
+
+const h1Style = {
+  margin: 0,
+  top: 'auto',
+  right: 8,
+  bottom: 16,
+  left: 'auto',
+  position: 'fixed'
+  // textAlign: 'center'
+}
 
 const paperStyle = {
   height: 60,
@@ -19,40 +46,19 @@ const paperStyle = {
   marginRight: 10
 };
 
-const dayStyle = {
-  textAlign: 'center',
-  marginRight: 20
-};
-
-const ambitStyle = {
-  float: 'left'
-}
-
-const h1Style = {
-  margin: 0,
-  top: 'auto',
-  right: 8,
-  bottom: 16,
-  left: 'auto',
-  position: 'fixed'
-  // textAlign: 'center'
-}
-
 
 const Day = (props) => {
   return (
     <Paper style={paperStyle} zDepth={2}>
-      <FloatingActionButton mini={false} backgroundColor='purple' /*onTouchTap={test}*/>
-        <div style={h1Style}>
-            <h1>{props.day}</h1>
-          </div>
+      <FloatingActionButton  
+        backgroundColor='purple' 
+        children={props.day}
+        /*onTouchTap={test}*/>
       </FloatingActionButton>
 
       <div>
         {props.ambitsOfDay.map((ambit, index) =>
-          <Ambit 
-            ambit={ambit} key={index}
-          />
+          <Ambit ambit={ambit} key={index}/>
         )}
       </div>
     </Paper>
