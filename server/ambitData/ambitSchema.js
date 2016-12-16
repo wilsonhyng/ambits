529b6ed7edbe5052c4106cffb69ee5ff969da905
Yoshi Oritatsu <yoshioritatsu@gmail.com>
2016-12-16 11:57:21 -0800

    (feature)ambitSchema - addStart time and icon to ambit schema

--- server/ambitData/ambitSchema.js
+++ server/ambitData/ambitSchema.js
@@ -11,8 +11,10 @@ var ambitSchema = new Schema({
   },
   weekdays: [Boolean], //0 is Sunday, 6 is Saturday
   startDate: Date,
+  startTime: Date,
   checkIns: [Date], // a history of successful check-ins
-  time: Date
+  time: Date,
+  icon: String
   //time (when during the day are you supposed to check in)
   //repeats (every week? every other week? is this necessary?)
 });
