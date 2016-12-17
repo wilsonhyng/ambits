7b3ce4e3b58c8fc76d57b86d655ebb99f55f8731
wilsonhyng <wilsonhyng@gmail.com>
2016-12-16 19:58:46 -0800

    (fix) Signup button reroutes to Day View

--- client/src/app/login/login.jsx
+++ client/src/app/login/login.jsx
@@ -60,7 +60,7 @@ class Login extends Component {
       this.props.main.setState({
         isLoggedIn: true
       });
-      browserHistory.push('/');
+      browserHistory.push('/day');
     })
     .catch(err => {
       const msg = err.response.data.message;
